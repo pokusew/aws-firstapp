055066e46e0a754a82d2d864850b2350fd628f6a
Martin Endler <pokusew@seznam.cz>
2015-01-30 20:19:48 +0100

    initial commit

---
+++ www/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>PHP app running on AWS</title>
+    </head>
+    <body>
+		<?php
+		echo 'ya!';
+		?>
+    </body>
+</html>
